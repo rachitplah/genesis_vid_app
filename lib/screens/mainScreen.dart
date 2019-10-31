a9b8454f5757d972dfb94a795f38ccdff069d6c5
Rachit Plah <rachitplah@outlook.com>
2019-11-01 01:15:45 +0530

    embedded login screen

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:genesis_vid_app/screens/loginScreen.dart';
+
+String useId="";
 
 class mScreen extends StatefulWidget{
   @override
@@ -9,16 +12,29 @@ class mScreen extends StatefulWidget{
 class mScreenState extends State<mScreen>{
   @override
   Widget build(BuildContext context) {
+    checkUseId();
     return Scaffold(
       appBar: AppBar(
               title: Center(
-                child: Text("lightbulb.in"),
+                child: Text("litebulb.in"),
               ),
       ),
       body: Material(
         child: Text("Hello"),
       ),
     );
+  }
+  void checkUseId()
+  async{ 
+    if(useId==null)
+     {await(loginId(context));
+      setState(() {});
+     }
+  }
+  void loginId(BuildContext context)async{
+     useId=await Navigator.push(context,MaterialPageRoute(builder: (context) {
+       return LoginScreen1();
+     }));
   }
 
 }
