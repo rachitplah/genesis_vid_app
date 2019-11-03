5db920d40ac1de956715110f9533d9f0fc2927b1
Rachit Plah <rachitplah@outlook.com>
2019-11-04 04:18:24 +0530

    main screen working done

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:genesis_vid_app/screens/loginScreen.dart';
+import 'dart:convert';
+import 'dart:async';
 
 String useId="";
 
@@ -10,10 +12,38 @@ class mScreen extends StatefulWidget{
   }
 }
 class mScreenState extends State<mScreen>{
+  List data;
+  /*
+  @override
+  void initState(){
+    super.initState();
+    print("object hhhh");
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async{
+       var response=//AssetBundle('assets/images/languages.jpg');
+       DefaultAssetBundle.of(context).loadString('assets/samplefiles/sample_list.json').toString();
+       setState(() {
+         var convertDataToJson=//json.decode(response);
+         jsonDecode(response);
+         data = convertDataToJson;
+         print(data);
+         /*
+         for(i=0;i < data.length;i++)
+         {    
+         if(data[i]['status']=="COMPLETED")
+         {
+             data.removeAt(i);
+             i--;
+         }
+         }*/
+       });
+       return "Success";
+  }*/
   @override
   Widget build(BuildContext context) {
     //checkUseId();
-    List data;
     return Scaffold(
       appBar: AppBar(
               title: //Text("litebulb.in"),
@@ -30,24 +60,61 @@ class mScreenState extends State<mScreen>{
     
       body: Material(
        // child: Text("Hello"),
-       child:     AnimatedList(
-                                     initialItemCount: data==null ? 0 : data.length,
-                                     itemBuilder: (BuildContext context,int index,Animation animation)
+       child:    FutureBuilder(
+         future: DefaultAssetBundle.of(context).loadString('assets/samplefiles/sample_list.json'),
+         builder: (context,snapshot){
+              var mydata=json.decode(snapshot.data.toString());
+              return ListView.builder(
+                                     //initialItemCount: data==null ? 0 : data.length,
+                                     itemCount: mydata==null ? 0 : mydata.length,
+                                     itemBuilder: (BuildContext context,int index)
                                      {
                                        return
+                                       GestureDetector(
+                                         onTap:(){
+                                              print("clicked clicked");
+                                         },
+                                         child:
                                         Container(
                                            child: 
                                              Card(
                                                 child:
-                                                    ListTile(
-                                                    leading: Icon(Icons.check_circle,),
-                                                    dense: true,
-                                                    title: Text(data[index]['description'],style: TextStyle(fontSize: 19.0),),
-                                                    trailing: Text("Hi"),
-                                                   ),),
+                                                    Column(
+                                                      children: <Widget>[
+                                                        Container(
+                                                          constraints: BoxConstraints.expand(
+                                                            height: 180.0,
+                                                          ),
+                                                          alignment: Alignment.bottomRight,
+                                                          padding: EdgeInsets.only(right: 16.0,bottom: 8.0),
+                                                          decoration: BoxDecoration(
+                                                            image: DecorationImage(
+                                                              image: NetworkImage(mydata[index]['image']),
+                                                              
+                                                              fit: BoxFit.cover,
+                                                            ),
+                                                          ),
+                                                          child: Text(mydata[index]['timer'],style: TextStyle(backgroundColor: Colors.black,color: Colors.white,),),
+                                                        ),
+                                                        ListTile(
+                                                          leading: Icon(Icons.person_pin,),
+                                                          dense: true,
+                                                          title: Text(mydata[index]['description'],style: TextStyle(fontSize: 19.0),),
+                                                          subtitle: Text(mydata[index]['channel']+" . "+mydata[index]['views']+" . "+mydata[index]['date'],),
+                                                          trailing: Icon(Icons.arrow_drop_down,),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    
+                                                   ),
+                                        ),
                                               );
                                       },
-                                   ),
+                                   );
+         },
+       ),
+       
+                 
       ),
     );
   }
