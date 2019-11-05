d9262b00d93d62330c1c7ebf677a6a0f3626a247
Rachit Plah <rachitplah@outlook.com>
2019-11-06 03:47:18 +0530

    Changes to get player screen working

--- lib/component/player.dart
+++ lib/component/player.dart
@@ -1,6 +1,61 @@
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 
+
+class VideoApp extends StatefulWidget {
+  var datid;
+  VideoApp(this.datid);
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
+
 class VideoPlayerScreen extends StatefulWidget {
   //VideoPlayerScreen({Key key}) : super(key: key);
   var datid;
@@ -20,7 +75,10 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     // Create and store the VideoPlayerController. The VideoPlayerController
     // offers several different constructors to play videos from assets, files,
     // or the internet.
-    _controller = VideoPlayerController.network(dataid,);
+    _controller = VideoPlayerController.network(//'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4'
+      dataid
+       //android:usesCleartextTraffic="true" part is added to allow establish http connections in android manifest
+      ,);
 
     // Initialize the controller and store the Future for later use.
     _initializeVideoPlayerFuture = _controller.initialize();
--- lib/screens/playerScreen.dart
+++ lib/screens/playerScreen.dart
@@ -13,6 +13,7 @@ class playerScreen extends StatefulWidget{
 class playerScreenState extends State<playerScreen>{
   var dataid;
   var mydata;
+  int ind=0;
   playerScreenState(this.dataid);
   @override
   Widget build(BuildContext context) {
@@ -28,7 +29,131 @@ class playerScreenState extends State<playerScreen>{
          builder: (context,snapshot){
               mydata=json.decode(snapshot.data.toString());
               return //Text("Hello $dataid");
-              VideoPlayerScreen(fun1(dataid));
+              Column(
+                children: <Widget>[
+                  
+                   Container(
+                           height:(MediaQuery.of(context).size.height)/4 ,
+                           width: MediaQuery.of(context).size.width ,
+                           child: 
+                                   VideoPlayerScreen(fun1(dataid)),
+                         ),
+                  Container(
+                    height: (MediaQuery.of(context).size.height)*(3/4),
+                    child:
+                  
+                  ListView(
+                    children: <Widget>[
+                      Container(
+                    child:
+                    Card(
+                      child:
+                     ListTile(
+                                                          dense: true,
+                                                          title: Text(mydata[ind]['description'],style: TextStyle(fontSize: 19.0),),
+                                                          subtitle: Text(mydata[ind]['views'],),
+                                                          trailing: Icon(Icons.arrow_drop_down,),
+                                                        ),
+                    ),
+                  ),
+                  Container(
+                    child:
+                    Card(
+                      child: 
+                     ListTile(
+                                                          leading: Icon(Icons.person_pin,),
+                                                          dense: true,
+                                                          title: Text(mydata[ind]['channel'],style: TextStyle(fontSize: 19.0),),
+                                                          
+                                                          trailing: 
+                                                          
+                                                              Text("SUBSCRIBE",style: TextStyle(fontSize: 15.0,fontWeight:FontWeight.w900, color: Colors.red,),),
+                                                          
+                                                              
+                                                        ),
+                    ),
+                  ),
+                  Container(
+                    height: 400.0,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.builder(
+                                     //initialItemCount: data==null ? 0 : data.length,
+                                     itemCount: mydata==null ? 0 : mydata.length,
+                                     itemBuilder: (BuildContext context,int index)
+                                     {
+                                       return
+                                       GestureDetector(
+                                         onTap:(){
+                                              print("clicked clicked");
+                                             // navigateToPlayer(mydata[index]['id']);
+                                         },
+                                         child:
+                                        Container(
+                                           child: 
+                                             Card(
+                                                child:
+                                                    Row(
+                                                      children: <Widget>[
+                                                        Container(
+                                                          constraints: BoxConstraints.expand(
+                                                            height: 90.0,
+                                                            width:  MediaQuery.of(context).size.width/2.05,
+                                                          ),
+                                                          alignment: Alignment.bottomRight,
+                                                          padding: EdgeInsets.only(right: 16.0,bottom: 8.0),
+                                                          decoration: BoxDecoration(
+                                                            image: DecorationImage(
+                                                              image: NetworkImage(mydata[index]['image']),
+                                                              
+                                                              fit: BoxFit.cover,
+                                                            ),
+                                                          ),
+                                                          child: Text(mydata[index]['timer'],style: TextStyle(backgroundColor: Colors.black,color: Colors.white,),),
+                                                        ),
+                                                        Container(
+                                                          height: 90.0,
+                                                          width: MediaQuery.of(context).size.width/2.05,
+                      
+                                                          child:
+                                                          
+                                                        ListTile(
+                                                          dense: true,
+                                                          
+                                                          title: Text(mydata[index]['description'],style: TextStyle(fontSize: 12.0),),
+                                                          subtitle: 
+                                                          Column(
+                                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                                            children: <Widget>[
+                                                          Text(mydata[index]['channel'],textAlign: TextAlign.right,),
+                                                          Text(mydata[index]['views'],),
+
+                                                           ],
+                                                          ),
+                                                          
+                                                         // isThreeLine: true,
+                                                          trailing: Icon(Icons.arrow_drop_down,),
+                                                        ),
+
+                                                          
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    
+                                                   ),
+                                        ),
+                                              );
+                                      },
+                                   ),
+                  ),
+
+                    ],
+                  ),
+                  ),
+                  
+                ],
+              );
+              
+              //VideoApp(dataid);
               //fun1(dataid);
          },
         ),
@@ -46,6 +171,7 @@ class playerScreenState extends State<playerScreen>{
           if(mydata[i]['id']==dataid)
           break;
         }
+        ind=i;
         var a=mydata[i]['url'];
         return a.toString();
     }
